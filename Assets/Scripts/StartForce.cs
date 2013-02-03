5178e197bb826e66be48a6c5cc3818d7b3ba1cc9
Plamen Chernev <plamen.chernev@marxmanstudio.com>
2013-02-03 20:50:31 +0200

    First commit

---
+++ Assets/Scripts/StartForce.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class StartForce : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log( "Start" );
+	
+		var force = new Vector3( 0.0f, 200.0f, -600.0f );
+		this.rigidbody.AddForce( force );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log( "Update" );
+	}
+	
+}
