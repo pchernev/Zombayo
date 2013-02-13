adaaf3fbb1412f58a965622dfc49f3e9f05c4983
Plamen Chernev <plamen.chernev@marxmanstudio.com>
2013-02-13 06:59:30 +0200

    Camera movement

---
+++ Assets/scrp.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class scrp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		var p = transform.position;
+		Camera.main.transform.position = new Vector3( Camera.main.transform.position.x, p.y, p.z );			
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var p = transform.position;
+		Camera.main.transform.position = new Vector3( Camera.main.transform.position.x, p.y, p.z );			
+	}
+}
