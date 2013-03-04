7002d047cbbdb18e8d7bf40404ebca53e9da3259
jazznazz <jazznazzz@gmail.com>
2013-03-04 05:14:13 -0800

    added some gameplay ideas

--- Documents/Zombayo project mindmap.mm
+++ Documents/Zombayo project mindmap.mm
@@ -1,35 +1,87 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1362245292573" ID="ID_960290015" MODIFIED="1362246527459" TEXT="project Zombayo" VGAP="0">
-<node CREATED="1362245307896" HGAP="96" ID="ID_198813170" MODIFIED="1362246495244" POSITION="left" TEXT="pre-production" VGAP="122" VSHIFT="-50">
-<node CREATED="1362245331356" HGAP="60" ID="ID_586271438" MODIFIED="1362246813123" TEXT="concept" VGAP="28" VSHIFT="17">
-<node CREATED="1362246499764" ID="ID_222217066" MODIFIED="1362246887690" TEXT="Gameplay" VSHIFT="26">
-<node CREATED="1362246541924" ID="ID_418166935" MODIFIED="1362246543656" TEXT="the player has control, not some random events">
-<node CREATED="1362246559236" HGAP="35" ID="ID_449631422" MODIFIED="1362246811161" TEXT="achieved by powerups like in &quot;&quot;go go goblin&quot;&quot;" VSHIFT="-21"/>
-</node>
-<node CREATED="1362246582435" ID="ID_1300708672" MODIFIED="1362246583952" TEXT="upgrades"/>
-<node CREATED="1362246874929" ID="ID_402058073" MODIFIED="1362246876397" TEXT="dr zitbag(or other character) holds Zombayo by the ears and kicks him"/>
-<node CREATED="1362246889001" ID="ID_85617149" MODIFIED="1362246890077" TEXT="close-ups when the rabbit hits something/someone in the air/ground"/>
-</node>
-<node CREATED="1362246814841" ID="ID_1412920316" MODIFIED="1362246824874" TEXT="ART">
-<node CREATED="1362246962736" ID="ID_1353685292" MODIFIED="1362246965868" TEXT="Concepts">
-<node CREATED="1362246967112" ID="ID_1964091272" MODIFIED="1362246985032" TEXT="concepts from dr. Zitbag&apos;s pet shop">
-<node CREATED="1362246976481" ID="ID_728509590" MODIFIED="1362246978053" TEXT="Zombayo"/>
-<node CREATED="1362246985032" ID="ID_1122267723" MODIFIED="1362246986109" TEXT="other creatures - many interesting to steal from"/>
-</node>
-</node>
+<node COLOR="#0033ff" CREATED="1362245292573" ID="ID_960290015" MODIFIED="1362402797456" TEXT="project Zombayo" VGAP="0">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="20"/>
+<node COLOR="#00b439" CREATED="1362245307896" HGAP="96" ID="ID_198813170" MODIFIED="1362402797456" POSITION="left" TEXT="pre-production" VGAP="122" VSHIFT="-50">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#990000" CREATED="1362245331356" HGAP="60" ID="ID_586271438" MODIFIED="1362402810824" TEXT="concept" VGAP="28" VSHIFT="17">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#111111" CREATED="1362246499764" HGAP="63" ID="ID_222217066" MODIFIED="1362402797456" TEXT="Gameplay" VSHIFT="-98">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1362246541924" ID="ID_418166935" MODIFIED="1362402797454" TEXT="the player has control, not some random events">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1362246559236" HGAP="36" ID="ID_449631422" MODIFIED="1362402481906" TEXT="achieved by powerups like in &quot;go go goblin&quot;" VSHIFT="-21">
+<edge COLOR="#111111" WIDTH="thin"/>
 </node>
 </node>
-<node CREATED="1362245333572" HGAP="80" ID="ID_356673014" MODIFIED="1362247029752" TEXT="prototype" VSHIFT="-10">
-<node CREATED="1362247003712" ID="ID_1612814490" MODIFIED="1362247014566" TEXT="&quot;fake&quot; of the game"/>
-<node CREATED="1362247029752" ID="ID_860567148" MODIFIED="1362247054855" TEXT="ART">
-<node CREATED="1362247046944" ID="ID_1885434595" MODIFIED="1362247047956" TEXT="hi-poly Zombayo">
-<node CREATED="1362247065224" ID="ID_1839157087" MODIFIED="1362247082004" TEXT="more mudbox training- modeling/texturing"/>
+<node COLOR="#111111" CREATED="1362246582435" ID="ID_1300708672" MODIFIED="1362402797455" TEXT="upgrades">
+<edge COLOR="#111111" WIDTH="thin"/>
 </node>
-<node CREATED="1362247054855" ID="ID_883272461" MODIFIED="1362247055892" TEXT="terrain"/>
+<node COLOR="#111111" CREATED="1362246874929" ID="ID_402058073" MODIFIED="1362402797455" TEXT="dr zitbag(or other character) holds Zombayo by the ears and kicks him">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1362246889001" ID="ID_85617149" MODIFIED="1362402797455" TEXT="close-ups when the rabbit hits something/someone in the air/ground">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1362402120118" ID="ID_1307350344" MODIFIED="1362402797455" TEXT="implement height meter">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1362402151071" ID="ID_1095972173" MODIFIED="1362402797455" TEXT="implement speedometer">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1362402157032" ID="ID_686533260" MODIFIED="1362402797455" TEXT="gradient background or other graphical thing to show us on what height are we">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1362402183520" ID="ID_1717479562" MODIFIED="1362402797455" TEXT="internal game clock , showing different animations for clashing into objects the longer you play - not to bore the player">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1362246814841" ID="ID_1412920316" MODIFIED="1362402797456" TEXT="ART">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1362246962736" ID="ID_1353685292" MODIFIED="1362402797455" TEXT="Concepts">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1362246967112" ID="ID_1964091272" MODIFIED="1362402481907" TEXT="concepts from dr. Zitbag&apos;s pet shop">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1362246976481" ID="ID_728509590" MODIFIED="1362402481907" TEXT="Zombayo">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1362246985032" ID="ID_1122267723" MODIFIED="1362402661790" TEXT="other creatures - many interesting to steal from">
+<edge COLOR="#111111" WIDTH="thin"/>
 </node>
 </node>
 </node>
-<node CREATED="1362245514291" HGAP="99" ID="ID_77616875" MODIFIED="1362246396773" POSITION="left" TEXT="production"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1362245333572" HGAP="80" ID="ID_356673014" MODIFIED="1362402797456" TEXT="prototype" VSHIFT="-10">
+<edge COLOR="#990000" WIDTH="1"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#111111" CREATED="1362247003712" ID="ID_1612814490" MODIFIED="1362402797456" TEXT="&quot;fake&quot; of the game">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#111111" CREATED="1362247029752" ID="ID_860567148" MODIFIED="1362402797456" TEXT="ART">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1362247046944" ID="ID_1885434595" MODIFIED="1362402797455" TEXT="hi-poly Zombayo">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1362247065224" ID="ID_1839157087" MODIFIED="1362402797453" TEXT="more mudbox training- modeling/texturing">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1362247054855" ID="ID_883272461" MODIFIED="1362402797455" TEXT="terrain">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1362245514291" HGAP="99" ID="ID_77616875" MODIFIED="1362402797457" POSITION="left" TEXT="production">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
 </node>
 </map>
