152d1b685b8c91600d994dd1ad17a8621920bb4c
jazznazz <jazznazzz@gmail.com>
2013-04-21 08:57:52 -0700

    changed scene facing direction to +Z, all MAX assets come facing this direction.

--- Assets/Scripts/MouseInput.cs
+++ Assets/Scripts/MouseInput.cs
@@ -38,7 +38,7 @@ public class MouseInput : MonoBehaviour
 				{
 					if( hit.rigidbody != null )
 					{
-						var force = new Vector3( 0.0f, 100.0f + dt * 3000.0f, -(550 + dt * 3000.0f) );
+						var force = new Vector3( 0.0f, 100.0f + dt * 3000.0f, (550 + dt * 3000.0f) );
 						hit.rigidbody.AddForce( force );
 					}
 				}
