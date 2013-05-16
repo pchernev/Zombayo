1fea761f22a6eac64f5080eeb54432e3b437af0f
Plamen Chernev <plamen.chernev@marxmanstudio.com>
2013-05-16 07:26:19 +0300

    Line renderer class.

--- Assets/Scripts/StartForce.cs
+++ Assets/Scripts/StartForce.cs
@@ -1,8 +1,84 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class StartForce : MonoBehaviour {
+[RequireComponent( typeof( FingerDownDetector ))]
+[RequireComponent( typeof( FingerMotionDetector ))]
+[RequireComponent( typeof( FingerUpDetector ))]
+[RequireComponent( typeof( ScreenRaycaster ))]
 
+public class StartForce : MonoBehaviour
+{
+	
+	class PathRenderer
+	{
+		LineRenderer lineRenderer;
+		
+		List<Vector3> points = new List<Vector3>();
+		List<GameObject> markers = new List<GameObject>();
+		
+		public PathRenderer( int index, LineRenderer lineRendererPrefab )
+		{
+			lineRenderer = Instantiate( lineRendererPrefab ) as LineRenderer;
+			lineRenderer.name = lineRendererPrefab.name + index;
+			lineRenderer.enabled = true;
+			
+			UpdateLines();
+		}
+		
+		public void Reset()
+		{
+			points.Clear();
+			UpdateLines();
+			
+			foreach( GameObject marker in markers )
+				Destroy( marker );
+			
+			markers.Clear();
+		}
+		
+		public void AddPoint( Vector2 screenPos )
+		{
+			AddPoint( screenPos, null );
+		}
+		
+		public void AddPoint( Vector2 screenPos, GameObject markerPrefab )
+		{
+			Vector3 pos = GetWorldPos( screenPos );
+			
+			if( markerPrefab )
+				AddMarker( pos, markerPrefab );
+			
+			points.Add( pos );
+			UpdateLines();
+		}
+		
+		GameObject AddMarker( Vector2 pos, GameObject prefab )
+		{
+			GameObject instance = Instantiate( prefab, pos, Quaternion.identity ) as GameObject;
+			instance.name = prefab.name + "(" + markers.Count + ")";
+			markers.Add( instance );
+			return instance;
+		}
+		
+		void UpdateLines()
+		{
+			lineRenderer.SetVertexCount( points.Count );
+			for( int i = 0; i < points.Count; ++i )
+				lineRenderer.SetPosition( i, points[i] );
+		}
+		
+		
+		public Vector3 GetWorldPos( Vector2 screenPos )
+		{
+			Ray ray = Camera.main.ScreenPointToRay( screenPos );
+			
+			float t = -ray.origin.z / ray.direction.z;
+			
+			return ray.GetPoint( t );
+		}
+	}
+	
 	// Use this for initialization
 	void Start () {
 		Debug.Log( "Start" );
