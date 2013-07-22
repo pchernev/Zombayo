d7cd15494ba6516b5f14f52519ab821fbaef8bce
jazznazz <jazznazzz@gmail.com>
2013-07-22 06:28:25 -0700

    created prefabs for first 4 terrain tiles - (0,0,0) no rotation, tweaked CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
+	public Vector3 cameraOffset = new Vector3(0,0,0) ;
+	public Vector3 cameraRotaion = new Vector3(0,0,0) ;
 	// Use this for initialization
 	void Start () 
 	{
 		var p = transform.position;
-		Camera.main.transform.position = new Vector3( p.x, p.y, Camera.main.transform.position.z );			
+		Camera.main.transform.position = new Vector3( p.x + cameraOffset.x, p.y + cameraOffset.y, p.z + cameraOffset.z );			
+		Camera.main.transform.eulerAngles = cameraRotaion;
+		
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		var p = transform.position;
-		Camera.main.transform.position = new Vector3( p.x, p.y, Camera.main.transform.position.z );			
+		Camera.main.transform.position = new Vector3( p.x + cameraOffset.x, p.y + cameraOffset.y, p.z + cameraOffset.z );			
+		Camera.main.transform.eulerAngles = cameraRotaion;
 	}
 }
