e5a62579a74440c57b7728aa557ccbb49f203f1c
Plamen Cherenv <pchernev@lendermobile.com>
2013-09-05 18:53:50 +0300

    First bounding box draw

--- Assets/Scripts/World/WorldPiece.cs
+++ Assets/Scripts/World/WorldPiece.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class WorldPiece : MonoBehaviour {
-
+	
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +13,49 @@ public class WorldPiece : MonoBehaviour {
 	void Update () {
 	
 	}
+	
+	void OnDrawGizmos() 
+	{
+		Bounds bound = new Bounds();
+		bool didOne = false;
+		bool found = false;
+		
+		didOne = GetBoundWithChildren( transform, ref bound, ref found );						
+		if( didOne )
+		{
+			iTween.DrawLineGizmos( new Vector3[] { bound.min, bound.max }, Color.red );
+		}
+	}
+	
+	bool GetBoundWithChildren( Transform parent, ref Bounds pBound, ref bool initBound )
+	{
+		Bounds bound = new Bounds();
+		bool didOne = false;
+		
+		if( parent.gameObject.renderer != null )
+		{
+			bound = parent.gameObject.renderer.bounds;
+			if( initBound )
+			{
+				pBound.Encapsulate( bound.min );
+				pBound.Encapsulate( bound.max );
+			}
+			else
+			{
+				pBound.min = new Vector3( bound.min.x, bound.min.y, bound.min.z );
+				pBound.max = new Vector3( bound.max.x, bound.max.y, bound.max.z );
+				initBound = true;
+			}
+			didOne = true;
+		}
+		
+		foreach( Transform child in parent )
+		{
+			if( GetBoundWithChildren( child, ref pBound, ref initBound ))
+				didOne = true;
+		}
+		
+		return didOne;
+	}
+	
 }
