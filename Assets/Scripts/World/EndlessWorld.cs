3370367dc1fe7885d05b53b6e6e2f676ae355921
Plamen Chernev <plamen.chernev@marxmanstudio.com>
2013-10-03 23:16:32 +0300

    Endless world Mecanim animation test

--- Assets/Scripts/World/EndlessWorld.cs
+++ Assets/Scripts/World/EndlessWorld.cs
@@ -11,7 +11,7 @@ public class EndlessWorld : MonoBehaviour
 	public GameObject[] prefabs;
 	
 	private List<GameObject> _objs;
-	private float cameraSize = 400.0f;
+	private float cameraSize = 150.0f;
 	
 	private GameObject _player;
 			
@@ -36,6 +36,7 @@ public class EndlessWorld : MonoBehaviour
 			Debug.Log( string.Format( "pos: {0} size: {1}", p.ToString(), piece.bounds.size.ToString() ));
 			
 //			mesh.enabled = false;
+			break;
 		}
 		
 		InitWorldPieces();
@@ -44,27 +45,57 @@ public class EndlessWorld : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		var fObj = _objs.FirstOrDefault();
-		var lObj = _objs.LastOrDefault();
+		var fObj = _objs.FirstOrDefault() as GameObject;
+		var lObj = _objs.LastOrDefault() as GameObject;
 		
-		var fMesh = fObj.GetComponent<MeshRenderer>();
-		var lMesh = lObj.GetComponent<MeshRenderer>();
+		var fX = fObj.transform.position.x;
+		var lX = lObj.transform.position.x;
 		
+		var fWorldPiece = fObj.GetComponent<WorldPiece>();
+		var lWorldPiece = lObj.GetComponent<WorldPiece>();
+		
+		var fBounds = fWorldPiece.getBounds();
+		var lBounds = lWorldPiece.getBounds();
+		
+		// camera position 
 		var lCamX = transform.position.x - cameraSize;
 		var rCamX = transform.position.x + cameraSize;
 		
-		if( fObj.transform.position.x + 100F < lCamX )
-			MoveRight();
-		else if( lObj.transform.position.x > rCamX )
-			MoveLeft();
+//		Debug.Log( string.Format( "lCamX: {0} rCamX: {1}, fBounds.min.x: {2}, lBounds.max.x: {3}", lCamX, rCamX, fBounds.min.x, lBounds.max.x ));
+//		Debug.Log( "_objs.Lenght: " + _objs.Count );
+		
+		// backward update
+		if( lCamX < fX )
+		{
+			var index = Random.Range( 0, prefabs.Length - 1);
+			var prefab = prefabs[index];
+			var piece = prefab.GetComponent<WorldPiece>();
+			var x = fObj.transform.position.x - piece.getBounds().size.x;
+			var pos = new Vector3( x, 0F, 0F );
 			
-//		if( Input.GetKeyDown( KeyCode.RightArrow ))
-//			MoveRight();
-//		else if( Input.GetKeyDown( KeyCode.LeftArrow ))			
-//			MoveLeft();
+			var obj = Instantiate( prefab, pos, prefab.gameObject.transform.rotation ) as GameObject;
+			_objs.Insert( 0, obj );
+			
+			Debug.Log( "backward x: " + x );
+		}
+		
+		// forward update
+		if( rCamX > lX )
+		{
+			var index = Random.Range( 0, prefabs.Length - 1);
+			var prefab = prefabs[index];
+			var piece = prefab.GetComponent<WorldPiece>();
+			var x = lX + fBounds.max.x + piece.getBounds().size.x * 0.5f;
+			var pos = new Vector3( x, 0F, 0F );
+			
+			var obj = Instantiate( prefab, pos, prefab.gameObject.transform.rotation ) as GameObject;
+			_objs.Add( obj );
+			
+			Debug.Log( "forward x: " + x );
+		}
 	}
 	
-	void MoveLeft()
+	void MoveBackward()
 	{
 		var fObj = _objs.FirstOrDefault();
 		var lObj = _objs.LastOrDefault();
@@ -76,7 +107,7 @@ public class EndlessWorld : MonoBehaviour
 //			obj.transform.position -= new Vector3( speed * Time.deltaTime, 0F, 0F );
 	}
 	
-	void MoveRight()
+	void MoveForward()
 	{
 		var fObj = _objs.FirstOrDefault();
 		var lObj = _objs.LastOrDefault();
