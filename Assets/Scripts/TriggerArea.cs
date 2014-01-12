25783e9ad951a538ba42844dd6cf9c032934e1f2
jazznazz <jazznazzz@gmail.com>
2014-01-12 15:57:50 +0200

    another try- completely reorganize all assets

---
+++ Assets/Scripts/TriggerArea.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerArea : MonoBehaviour {
+	
+	public DrController doctor;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter( Collider collider )
+	{
+		var obj = collider.gameObject;
+		//Debug.Log( "obj.tag: " + obj.tag );
+		if( obj.tag == "Doctor" )
+			doctor.SetKickZone( true );
+	}
+	
+	void OnTriggerExit( Collider collider )
+	{
+		var obj = collider.gameObject;
+		if( obj.tag == "Doctor" )
+			doctor.SetKickZone( false );
+	}
+	
+	void OnCollisionEnter( Collision collision )
+	{
+		Debug.Log( "Trigger->OnCollisionEnter: " );
+	}
+}
