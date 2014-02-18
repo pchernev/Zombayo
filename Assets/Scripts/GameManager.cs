8da04a875f14d97821790250aeb1620bf2906211
krstan4o <krstan4o@yahoo.com>
2014-02-19 01:38:46 +0200

    [add] added some helper methods in the GameManager.cs for enabling/disabling ingame Panels(Menus, Scores, Shop) ...

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,42 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    Dictionary<string, GameObject> gamePanels = new Dictionary<string, GameObject>();
+	void Start ()
+    {
+        loadAllPanels();
+        disablePanelsExcept("In Game");
+	}
+	   
+	void Update ()
+    {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void FixedUpdate() 
+    {
+        
+    }
+
+    private void loadAllPanels()
+    {
+        var panelsHolder = GameObject.Find("Anchor").transform;
+        foreach (Transform child in panelsHolder)
+        {
+            gamePanels.Add(child.transform.name, child.transform.gameObject);
+        }
+    }
+
+    private void disablePanelsExcept(string panelNames)
+    {
+        var namesSplited = panelNames.Split(new string[] { ", "}, StringSplitOptions.RemoveEmptyEntries);
+
+       gamePanels.Where(x => !namesSplited.Contains(x.Key) && gamePanels.ContainsKey(x.Key) && x.Value.active == true)
+            .ToList<KeyValuePair<string, GameObject>>().ForEach(x => { x.Value.active = false; });
+    }
 }
