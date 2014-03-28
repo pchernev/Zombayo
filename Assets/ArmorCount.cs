a8228f0eb447514ac8c5946d839e94236b01ab75
Tsvetan Chipilski <tsvetan.chipilski@yahoo.com>
2014-03-28 13:18:03 +0200

    InGame UI completed

---
+++ Assets/ArmorCount.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ArmorCount : MonoBehaviour {
+	GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+		_player = GameObject.FindWithTag("Player");
+		var item = _player.GetComponent<Player>().gameData.ShopItems.FirstOrDefault(x => x.Name == "Bladder");
+		var ammountOfArmors = item.UpgradesCount;
+		UILabel label = GetComponent<UILabel> ();
+		label.text = "";
+		label.text += ammountOfArmors.ToString ();
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
