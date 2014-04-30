ffa8b343107666231c21c5c103bc360ed4fd17db
Quicks01ver <chichoitzo@hotmail.com>
2014-04-30 17:07:36 +0300

    Added App Intro scene with placeholder video playback setup

---
+++ Assets/Videos/PlayVideo.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideo : MonoBehaviour
+{
+  public MovieTexture movieTexture;
+
+  void Start() {
+    if (movieTexture != null)
+      movieTexture.Play();
+	}
+}
