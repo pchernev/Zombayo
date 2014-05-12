5fcb001c1705dc556f84c086b6ace6d27c55be60
Plamen Chernev <plamen.chernev@marxmanstudio.com>
2014-05-12 10:51:06 +0300

    Start game logic

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,230 +4,225 @@ using System.Collections;
 
 public class GameLogic : MonoBehaviour
 {
-  public static GameLogic Instance;
+	public static GameLogic Instance;
+	public GameData gameData;
+	public UpgradeData upgradeData;
+	public UIPanel inGameGUIPanel;
+	public Animation mainMenuAnim;
+	public Animation endGameAnim;
+	public CameraFollow cameraFollow;
+	public StartPrefabs startPrefabs;
+	public GameObject mainChars;
+	[HideInInspector]
+	public DrFishhead doctor;
+	[HideInInspector]
+	public Player player;
 
-  public GameData gameData;
-  public UpgradeData upgradeData;
-  public UIPanel inGameGUIPanel;
-  public Animation mainMenuAnim;
-  public Animation endGameAnim;
-  public CameraFollow cameraFollow;
+	public bool IsPlayerActive { get { return gameData.IsPlayerKicked && !gameOver; } }
 
-  public StartPrefabs startPrefabs;
+	public KickSwipe swipe;
+	private bool gameOver;
 
-  public GameObject mainChars;
-  [HideInInspector]
-  public DrFishhead doctor;
-  [HideInInspector]
-  public Player player;
-  public bool IsPlayerActive { get { return gameData.IsPlayerKicked && !gameOver; } }
-
-  public KickSwipe swipe;
-
-  private bool gameOver;
-
-  void Awake()
-  {
-    Instance = this;
-
-    player = mainChars.GetComponentInChildren<Player>();
-    doctor = mainChars.GetComponentInChildren<DrFishhead>();
-
-    if (gameData.ZeroProgress || !loadGame())
-      resetProgress();
-  }
-    
-	void Start()
+	void Awake ()
 	{
-    showStartScene();
-
-    inGameGUIPanel.enabled = false;
-    mainMenuAnim.gameObject.SetActive(true);
-  }
+		Instance = this;
 	
-	void Update()
-  {
-    if (!gameOver && gameData.IsPlayerKicked) {
-      checkForLevelComplete();
-      updatePlayerState();
-      checkForLowSpeedGameOver();
-    }
-  }
+		player = mainChars.GetComponentInChildren<Player> ();
+		doctor = mainChars.GetComponentInChildren<DrFishhead> ();
 
+		if (gameData.ZeroProgress || !loadGame ())
+			resetProgress ();
+	}
 
-  public void showStartScene()
-  {
-    player.moveToStartSpot();
-    mainChars.SetActive(false);
-    startPrefabs.recycleOld();
-    startPrefabs.spawn();
-    cameraFollow.reset();
-  }
+	void Start ()
+	{
+		showStartScene ();
 
-  public void startGame()
-  {
-    showStartScene();
-    loadGame();
-    mainChars.SetActive(true);
-    doctor.prepareForStart();
-    player.prepareForStart();
-    gameData.setFromLevels();
-    gameOver = false;
+		inGameGUIPanel.enabled = false;
+		mainMenuAnim.gameObject.SetActive (true);
 
-    inGameGUIPanel.enabled = true;
-    mainMenuAnim.gameObject.SetActive(false);
-    mainMenuAnim["igm_show"].time = 0;
-    endGameAnim.gameObject.SetActive(false);
-    endGameAnim["igm_show"].time = 0;
+		resetProgress();
+		startGame();
+	}
 
-    swipe.gameObject.SetActive(true);
+	void Update ()
+	{
+		if (!gameOver && gameData.IsPlayerKicked) {
+			checkForLevelComplete ();
+			updatePlayerState ();
+			checkForLowSpeedGameOver ();
+		}
+	}
 
-    gameData.kickTime = -1;
-    Time.timeScale = 1;
-  }
+	public void showStartScene ()
+	{
+		player.moveToStartSpot ();
+		mainChars.SetActive (false);
+		startPrefabs.recycleOld ();
+		startPrefabs.spawn ();
+		cameraFollow.reset ();
+	}
 
-  public void pauseGame()
-  {
-    Time.timeScale = 0;
-    swipe.gameObject.SetActive(false);
-  }
+	public void startGame ()
+	{
+		showStartScene ();
+		loadGame ();
+		mainChars.SetActive (true);
+		doctor.prepareForStart ();
+		player.prepareForStart ();
+		gameData.setFromLevels ();
+		gameOver = false;
 
-  public void resumeGame()
-  {
-    Time.timeScale = 1;
-    swipe.gameObject.SetActive(!gameData.IsPlayerKicked);
-  }
+		inGameGUIPanel.enabled = true;
+		mainMenuAnim.gameObject.SetActive (false);
+		mainMenuAnim ["igm_show"].time = 0;
+		endGameAnim.gameObject.SetActive (false);
+		endGameAnim ["igm_show"].time = 0;
 
-  public void endGame()
-  {
-    player.rigidbody.isKinematic = true;
-    gameOver = true;
+		swipe.gameObject.SetActive (true);
 
-    inGameGUIPanel.enabled = false;
-    endGameAnim.gameObject.SetActive(true);
+		gameData.kickTime = -1;
+		Time.timeScale = 1;
+	}
 
-    gameData.coinCount += (int) (0.001f * gameData.travelledDistance * (float)gameData.coinsPerKm);
-    saveGame();
-  }
+	public void pauseGame ()
+	{
+		Time.timeScale = 0;
+		swipe.gameObject.SetActive (false);
+	}
 
-  public void delayedEndGame(float delay)
-  {
-    StartCoroutine(EndGameRoutine(delay));
-  }
+	public void resumeGame ()
+	{
+		Time.timeScale = 1;
+		swipe.gameObject.SetActive (!gameData.IsPlayerKicked);
+	}
 
-  public void goToMainMenu()
-  {
-    inGameGUIPanel.enabled = false;
-    mainMenuAnim.gameObject.SetActive(true);
-    showStartScene();
-  }
+	public void endGame ()
+	{
+		player.rigidbody.isKinematic = true;
+		gameOver = true;
 
-  public void finishLevel()
-  {
-    endGame();
-  }
+		inGameGUIPanel.enabled = false;
+		endGameAnim.gameObject.SetActive (true);
 
-  public void saveGame()
-  {
-    string levels = "";
-    for (int i = 0; i < (int)UpgradeLevel.Type.Count; ++i)
-      levels += (char)gameData.levels[i];
+		gameData.coinCount += (int)(0.001f * gameData.travelledDistance * (float)gameData.coinsPerKm);
+		saveGame ();
+	}
 
-    PlayerPrefs.SetString("levels", levels);
-    PlayerPrefs.SetInt("coins", gameData.coinCount);
-  }
+	public void delayedEndGame (float delay)
+	{
+		StartCoroutine (EndGameRoutine (delay));
+	}
 
-  public bool loadGame()
-  {
-    string levels = PlayerPrefs.GetString("levels");
-    if (levels.Length == (int)UpgradeLevel.Type.Count)
-      for (int i = 0; i < (int)UpgradeLevel.Type.Count; ++i)
-        gameData.levels[i] = (int)levels[i];
-    else
-      return false;
+	public void goToMainMenu ()
+	{
+		inGameGUIPanel.enabled = false;
+		mainMenuAnim.gameObject.SetActive (true);
+		showStartScene ();
+	}
 
-    gameData.coinCount = PlayerPrefs.GetInt("coins");
+	public void finishLevel ()
+	{
+		endGame ();
+	}
 
-    return true;
-  }
+	public void saveGame ()
+	{
+		string levels = "";
+		for (int i = 0; i < (int)UpgradeLevel.Type.Count; ++i)
+				levels += (char)gameData.levels [i];
 
+		PlayerPrefs.SetString ("levels", levels);
+		PlayerPrefs.SetInt ("coins", gameData.coinCount);
+	}
 
-  public void initiateKick()
-  {
-    player.UpdateKickEfficiency = false;
-    doctor.playRunAndKickSequence();
-  }
+	public bool loadGame ()
+	{
+		string levels = PlayerPrefs.GetString ("levels");
+		if (levels.Length == (int)UpgradeLevel.Type.Count)
+				for (int i = 0; i < (int)UpgradeLevel.Type.Count; ++i)
+						gameData.levels [i] = (int)levels [i];
+		else
+				return false;
 
-  public void initiateKickInCustomDirection(Vector3 direction)
-  {
-    gameData.kickDirection = direction;
-    initiateKick();
-  }
+		gameData.coinCount = PlayerPrefs.GetInt ("coins");
 
-  public void launchRabbit()
-  {
-    doctor.rigidbody.isKinematic = true;
-    player.kickRabbit(gameData.KickForce);
-    gameData.kickTime = Time.time;
+		return true;
+	}
 
-    cameraFollow.doTransition = true;
-  }
+	public void initiateKick ()
+	{
+		player.UpdateKickEfficiency = false;
+		doctor.playRunAndKickSequence ();
+	}
 
-  public void resetProgress()
-  {
-    gameData.coinCount = 0;
-    for (int i = 0; i < gameData.levels.Length; ++i)
-      gameData.levels[i] = 0;
-    
-    saveGame();
-  }
+	public void initiateKickInCustomDirection (Vector3 direction)
+	{
+		gameData.kickDirection = direction;
+		initiateKick ();
+	}
 
+	public void launchRabbit ()
+	{
+		doctor.rigidbody.isKinematic = true;
+		player.kickRabbit (gameData.KickForce);
+		gameData.kickTime = Time.time;
 
-  private void updatePlayerState()
-  {
-    if (player.IsFarting) {
-      gameData.fartTime -= Time.deltaTime;
-      checkIfFartUsedUp();
-    }
-    else if (player.IsGliding) {
-      gameData.glideTime -= Time.deltaTime;
-      checkIfGlideUsedUp();
-    }
-  }
+		cameraFollow.doTransition = true;
+	}
 
-  private void checkForLowSpeedGameOver()
-  {
-    if (gameData.kickTime + 0.1f < Time.time && player.IsTooSlow)
-      endGame();
-  }
+	public void resetProgress ()
+	{
+		gameData.coinCount = 0;
+		for (int i = 0; i < gameData.levels.Length; ++i)
+			gameData.levels [i] = 0;
 
-  private void checkForLevelComplete()
-  {
-    if (gameData.travelledDistance >= gameData.distanceToFinish)
-      finishLevel();
-  }
+		saveGame ();
+	}
 
-  private void checkIfFartUsedUp()
-  {
-    if (player.IsFarting && gameData.fartTime <= 0) {
-      gameData.fartTime = 0;
-      player.enterPowerUpState(Player.PowerUpState.None);
-    }
-  }
+	private void updatePlayerState ()
+	{
+		if (player.IsFarting) {
+			gameData.fartTime -= Time.deltaTime;
+			checkIfFartUsedUp ();
+		} else if (player.IsGliding) {
+			gameData.glideTime -= Time.deltaTime;
+			checkIfGlideUsedUp ();
+		}
+	}
 
-  private void checkIfGlideUsedUp()
-  {
-    if (player.IsGliding && gameData.glideTime <= 0) {
-      gameData.glideTime = 0;
-      player.enterPowerUpState(Player.PowerUpState.None);
-    }
-  }
+	private void checkForLowSpeedGameOver ()
+	{
+		if (gameData.kickTime + 0.1f < Time.time && player.IsTooSlow)
+			endGame ();
+	}
 
+	private void checkForLevelComplete ()
+	{
+		if (gameData.travelledDistance >= gameData.distanceToFinish)
+			finishLevel ();
+	}
 
-  private IEnumerator EndGameRoutine(float delay)
-  {
-    yield return new WaitForSeconds(delay);
+	private void checkIfFartUsedUp ()
+	{
+		if (player.IsFarting && gameData.fartTime <= 0) {
+			gameData.fartTime = 0;
+			player.enterPowerUpState (Player.PowerUpState.None);
+		}
+	}
 
-    endGame();
-  }
+	private void checkIfGlideUsedUp ()
+	{
+		if (player.IsGliding && gameData.glideTime <= 0) {
+			gameData.glideTime = 0;
+			player.enterPowerUpState (Player.PowerUpState.None);
+		}
+	}
+
+	private IEnumerator EndGameRoutine (float delay)
+	{
+		yield return new WaitForSeconds (delay);
+
+		endGame ();
+	}
 }
